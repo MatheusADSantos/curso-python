0df88f7e23ca65079782112f1400b875f67593e0
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-03-24 23:56:36 -0300

    How to use string with print()

---
+++ aula3/aula3.py
@@ -0,0 +1,14 @@
+""""
+All wirte inside """""" are string
+"""
+
+# The most common to use ...
+print("This a 'string' (str).")
+print('This a "string" (str).')
+
+# Here we are using character '\' to scape like below
+print("This my \"text\"  (str).")
+print('This my \'text\' (str).')
+
+# When we need only print, don't execute nothing more, we can use param 'r'
+print(r"This my text with \n not break line")
