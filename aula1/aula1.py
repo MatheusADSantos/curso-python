88d638e15c271212eb5a779c1e404dbccd69a27e
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-03-24 23:16:31 -0300

    First Commit with Python

---
+++ aula1/aula1.py
@@ -0,0 +1,15 @@
+"""
+It will use to documentation ...
+
+print('Linha 1')
+print('Linha 2')
+print('Linha 3')
+"""
+
+'''
+Another type comment ...
+'''
+
+print('Linha 4')  # The comments must have 2 spaces from code ...
+
+# print('Linha 5')
---
+++ aula2/aula2.py
@@ -0,0 +1,9 @@
+print("In this class we'll learn about 'sep' and 'end' params from function print ...")
+
+''''
+Reproduce this CEP with print
+'''
+
+print("824", "176", "070", sep=".", end="-")
+print("18")
+
